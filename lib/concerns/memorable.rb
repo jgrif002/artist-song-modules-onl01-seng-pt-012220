614fa079be7c8d0651f6b035441a9a08ea30e799
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-25 03:50:52 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable 
-  module 
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
-  end
+    def count
+      self.all.count
+    end
+  end 
+  
+  module InstanceMethods
+    
+  end 
   
 end 
