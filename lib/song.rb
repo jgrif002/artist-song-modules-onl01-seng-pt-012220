ce042cdbb8606f79e0620f9140c2fb506cfdb7af
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-25 04:20:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,6 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
   end
@@ -26,5 +22,5 @@ class Song
     name.downcase.gsub(' ', '-')
   end
   
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods, findable 
 end 
